c213c6a624735bf684a125fa067160ac2c80c1c7
Renan Dias - Grilo_Sueco <renan.diaz@gmail.com>
2018-05-04 20:20:49 -0300

    Desafio 28

---
+++ aula_10.py
@@ -0,0 +1,33 @@
+# aula10
+#tempo = int(input('Quantos anos tem seu carro?'))
+#if tempo<=3:
+#    print('Carro novo')
+#else:
+#    print('Carro velho')
+#print('--FIM--')
+
+#nome = str(input('Qual é o seu nome? '))
+#if nome == 'Renan':
+#    print('Quem nome bonito você tem!')
+#else:
+#    print('Seu nome é tão normal!')
+#print('Bom dia {}!'.format(nome))
+
+#n1 = float(input('Digite a primeira nota: '))
+#n2 = float(input('Digite a segunda: '))
+#m = (n1+n2)/2
+#if m<6:
+#    print('A sua média de {:.1f}, não foi boa!'.format(m))
+#else:
+#    print('A sua média de {:.1f}, foi boa, parabéns!'.format(m))
+
+# Desafio 28
+#import random
+#guess = int(input('De 0 a 5, tente adivinhar qual número será sorteado! '))
+#num = random.randrange(5)
+#if guess == num:
+#    print('Parabéns você acertou!')
+#else:
+#    print('Puxa não foi dessa vez, o número sorteado foi: {}'.format(num))
+
+# Desafio 29
--- aulas_gerais.py
+++ aulas_gerais.py
@@ -159,10 +159,3 @@
 
 # 22
 
-# aula10
-tempo = int(input('Quantos anos tem seu carro?'))
-if tempo<=3:
-    print('Carro novo')
-else:
-    print('Carro velho')
-print('--FIM--')
