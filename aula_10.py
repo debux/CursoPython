87edd6a8b6e7b4fc0f4abaa7dae01b1c749ba7a1
Renan Dias - Grilo_Sueco <renan.diaz@gmail.com>
2018-05-08 20:26:10 -0300

    Desafio 35 fim da aula 10

--- aula_10.py
+++ aula_10.py
@@ -48,12 +48,62 @@
 #    print('O número escolhido é ÍMPAR!')
 
 # Desafio 31
-km = int(input('Insira em kilometros a distancia da viagem: '))
-pass200 = (0,50 * km)
-passmore = (0,45 * km)
-if km <= 200:
-    print('O valor da passagem é: {}'.format(pass200))
-    5-+.+69
+#km = int(input('Insira em kilometros a distancia da viagem: '))
+#pass200 = (0.50 * km)
+#passmore = (0.45 * km)
+#if km <= 200:
+#    print('O valor da passagem é: {0:,.2f}'.format(pass200))
+#else:
+#    print('O valor da passage, é: {0:,.2f}'.format(passmore))
 
-else:
-    print('O valor da passage, é: {}'.format(passmore))
+# Desafio 32
+#Modo simples:
+#from datetime import date
+#ano = int(input('Que ano quer analisar? Coloque 0 para o ano atual: '))
+#if ano == 0:
+#    ano = date.today().year
+#if ano%4 == 0 and ano%100 !=0 or ano%400 ==0:
+#    print('O ano {} é Bissexto'.format(ano))
+#else:
+#    print('O ano {} não é Bissexto'.format(ano))
+
+#modo complexo
+#import calendar
+#ano = int(input('Insira o ano: '))
+#if calendar.isleap(ano) == True:
+#    print('O ano {} é Bissexto')
+#else:
+#    print('Ano não Bissexto')
+
+# Desafio 33
+#num1 = int(input('insira o primeiro número: '))
+#num2 = int(input('insira o segundo número: '))
+#num3 = int(input('insira o terceiro número: '))
+#menor = num1
+#if num2 < num1 and num2 < num3:
+#    menor = num2
+#if num3 < num1 and num3 < num2:
+#    menor = num3
+#print('O menor valor digitado foi {}'.format(menor))
+#maior = num1
+#if num2 > num1 and num2 > num3:
+#    menor = num2
+#if num3 > num1 and num3 > num2:
+#    maior = num3
+#print('O maior valor digitado foi {}'.format(maior))
+
+# Desafio 34
+#sal = int(input('Insira seu salário: '))
+#if sal >= 1250:
+#    print('Você terá um aumento de 10%, ficando em R${:.2f}:'.format(sal+sal*10/100))
+#else:
+#    print('Você terá um aumento de 15%, ficando em R${:.2f}:'.format(sal+sal*15/100))
+
+# Desafio 35
+#c1 = int(input('Insira o comprimento da primeira reta: '))
+#c2 = int(input('Insira o comprimento da segunda reta: '))
+#c3 = int(input('Insira o comprimento da terceira reta: '))
+#if c1 < c2 + c3 and c2 < c1 + c3 and c3 < c1 + c2:
+#    print('Com os comprimentos apresentados, é possível criar um triangulo!')
+#else:
+#    print('Infelizmente com os comprimentos apresentados não é possível criar um triangulo!')
