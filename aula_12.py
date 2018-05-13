9bdf6d9b410dfb12a99ca384b37320a3494496a5
Debux <renan.diaz@gmail.com>
2018-05-13 01:12:43 -0300

    Desafio 30

--- aula_12.py
+++ aula_12.py
@@ -15,10 +15,12 @@
 
 val = float(input('Insira o valor da casa: '))
 sal = float(input('Insira seu salário: '))
-anos = int(input('Insira em quantos irá pagar: '))
-prest = ((sal * anos) /100)
-limit = (sal * 30) / 100)
-if prest < limit:
-    print('O Valor da prestação mensal é de: {:.3f}'.format(prest))
-elif prest >= limit:
-    print('O Valor da prestação mensal está acima do permitido {:.3f}'.format(limit))
+anos = int(input('Insira em quantos anos irá pagar: '))
+prest = (val / (anos*12))
+limit = ((sal * 30) / 100)
+print('prestação: {%.2f} e limite: {:.3f}'.format(prest,limit))
+#if prest <= limit:
+#    print('O Valor da prestação mensal é de: {:.3f}'.format(prest))
+#else:
+#    #prest > limit:
+#    print('O Valor da prestação mensal {:.3f} está acima do permitido: {:.3f}'.format(prest, limit))
